8d677b9e848d600a1a0b3376a7af00f508c7e7c5
TakeAsh <take@TakeAsh.net>
2017-06-15 03:59:36 +0900

    fix: fail to change when there is only 1 network

--- ChangeNetCategory.ps1
+++ ChangeNetCategory.ps1
@@ -3,25 +3,26 @@
 # You have to run this script as an administrator.
 
 Get-WmiObject Win32_NetworkAdapterConfiguration |
+  ? {$_.InterfaceIndex -ne $null} |
   % {$adptors = @{}} {$adptors[$_.InterfaceIndex] = $_}
-$networks = Get-NetConnectionProfile |
-  Sort-Object Name |
-  % {$index = 0} {
-  $adaptor = $adptors[$_.InterfaceIndex];
-  [PSCustomObject] @{
-    Index = $index++;
-    Profile = $_;
-    Name = $_.Name;
-    NetworkCategory = $_.NetworkCategory;
-    Description = $adaptor.Description;
-    DHCPEnabled = $adaptor.DHCPEnabled;
-    IPAddress = $adaptor.IPAddress;
-    IPSubnet = $adaptor.IPSubnet;
-    DefaultIPGateway = $adaptor.DefaultIPGateway;
-    MACAddress = $adaptor.MACAddress;
-    DNSDomain = $adaptor.DNSDomain;
-  }
-}
+$networks = @( Get-NetConnectionProfile |
+    Sort-Object Name |
+    % {$index = 0} {
+    $adaptor = $adptors[$_.InterfaceIndex];
+    [PSCustomObject] @{
+      Index            = $index++;
+      Profile          = $_;
+      Name             = $_.Name;
+      NetworkCategory  = $_.NetworkCategory;
+      Description      = $adaptor.Description;
+      DHCPEnabled      = $adaptor.DHCPEnabled;
+      IPAddress        = $adaptor.IPAddress;
+      IPSubnet         = $adaptor.IPSubnet;
+      DefaultIPGateway = $adaptor.DefaultIPGateway;
+      MACAddress       = $adaptor.MACAddress;
+      DNSDomain        = $adaptor.DNSDomain;
+    }
+  })
 $networks |
   Format-List -Property Index, Name, NetworkCategory, Description, DHCPEnabled, IPAddress, IPSubnet, DefaultIPGateway, MACAddress, DNSDomain
 
