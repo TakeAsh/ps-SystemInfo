4a39fe14774fa61d9482caf425dcb58149e8956e
TakeAsh <take@TakeAsh.net>
2017-08-17 00:44:24 +0900

    add "GetUsbDevices.ps1"

---
+++ GetUsbDevices.ps1
@@ -0,0 +1,6 @@
+﻿# [Displaying USB Devices using WMI | PowerShell Team Blog](https://blogs.msdn.microsoft.com/powershell/2007/02/24/displaying-usb-devices-using-wmi/)
+
+Get-WmiObject Win32_USBControllerDevice |
+  % {[wmi]($_.Dependent)} |
+  sort PNPClass, Service, Description, DeviceID |
+  Format-Table -auto PNPClass, Service, Description, DeviceID
