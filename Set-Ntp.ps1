074cdda5b524c362acd481b383da47ab2b2e2ea3
TakeAsh <take@TakeAsh.net>
2017-12-09 21:57:28 +0900

    Set-Ntp: Get/Set Interval

--- Set-Ntp.ps1
+++ Set-Ntp.ps1
@@ -1,16 +1,20 @@
 # Get/Set NTP Server
-# [setting NTP server on Windows machine using PowerShell - Stack Overflow](https://stackoverflow.com/questions/17507339/)
-#
-# If you want to delete a server from the server list, enter the command like below.
-# > Remove-ItemProperty -LiteralPath $ntpServersPath -Name "3"
+# - This script require the administrator privilege.
+# - [setting NTP server on Windows machine using PowerShell - Stack Overflow](https://stackoverflow.com/questions/17507339/)
+# - If you want to delete a server from the server list, enter the command like below.
+#   > Remove-ItemProperty -LiteralPath $ntpServersPath -Name "3"
 
 $ntpServersPath = "HKLM:/SOFTWARE/Microsoft/Windows/CurrentVersion/DateTime/Servers"
 $w32timeParametersPath = "HKLM:/SYSTEM/CurrentControlSet/services/W32Time/Parameters"
+$w32timeNtpClientPath = "HKLM:/SYSTEM/CurrentControlSet/services/W32Time/TimeProviders/NtpClient"
+$hourToSec = 60.0 * 60
 $servers = Get-Item -LiteralPath $ntpServersPath
 
 function Get-ServerIndexes() {
+  $index = 0
   $servers.GetValueNames() |
     ? {$_ -ne ""} |
+    % {$(if ([int]::TryParse($_, [ref]$index)) {$index} else {0})} |
     Sort-Object
 }
 
@@ -20,24 +24,45 @@ function Show-Servers() {
     % {$(if ($_ -eq $selected) {"*"} else {" "}) + $_ + ": " + $servers.GetValue($_)}
 }
 
-$serverIndexes = Get-ServerIndexes
+function Show-Interval() {
+  $value = (Get-ItemProperty -LiteralPath $w32timeNtpClientPath).SpecialPollInterval / $hourToSec
+  Write-Output "Interval (hours): ${value}" 
+}
+
 Show-Servers
-$in = Read-Host "Select index, or enter new NTP server host"
-if ($in -eq "") {
+Show-Interval
+$in1 = Read-Host "Select index, or enter new NTP server host"
+if ($in1 -eq "") {
   Write-Host "Canceled."
   Exit
 }
+$maxIndex = @(Get-ServerIndexes)[-1]
 $serverIndex = 0
 if (
-  -not [int]::TryParse($in, [ref]$serverIndex) -or
+  -not [int]::TryParse($in1, [ref]$serverIndex) -or
   $serverIndex -lt 0 -or
-  $serverIndex -ge $serverIndexes.Length
+  $serverIndex -gt $maxIndex
 ) {
-  $serverIndex = $serverIndexes.Length
-  New-ItemProperty -LiteralPath $ntpServersPath -Name $serverIndex -Value $in | Out-Null
+  $serverIndex = $maxIndex + 1
+  New-ItemProperty -LiteralPath $ntpServersPath -Name $serverIndex -Value $in1 | Out-Null
 }
 Set-ItemProperty -LiteralPath $ntpServersPath -Name "(Default)" -Value $serverIndex
 Set-ItemProperty -LiteralPath $w32timeParametersPath -Name "NtpServer" -Value $servers.GetValue($serverIndex)
 Show-Servers
+Stop-Service w32time
+Start-Service w32time
+
+Show-Interval
+$in2 = Read-Host "Enter Interval (hours)"
+$interval = 0
+if (
+  -not [double]::TryParse($in2, [ref]$interval) -or
+  $interval -le 0
+) {
+  Write-Host "Canceled."
+  Exit
+}
+Set-ItemProperty -LiteralPath $w32timeNtpClientPath -Name "SpecialPollInterval" -Value ($interval * $hourToSec)
+Show-Interval
 Stop-Service w32time
 Start-Service w32time
