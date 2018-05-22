183b14de526d55f398e2dd72094ce5cc9f076181
TakeAsh <take@TakeAsh.net>
2018-05-22 22:31:39 +0900

    add AddSystemPath.ps1

---
+++ AddSystemPath.ps1
@@ -0,0 +1,20 @@
+﻿# add path items to system path, remove duplicated items
+$addingPathItems = @(
+  'C:\Program Files\7-Zip',
+  'C:\Program Files\Git\usr\bin',
+  'C:\Program Files (x86)\Microsoft Visual Studio 14.0\VC\bin', # nmake.exe
+  'C:\tools\selenium'
+)
+$registryEnvironment = 'HKLM:/SYSTEM/CurrentControlSet/Control/Session Manager/Environment'
+
+$identity = [Security.Principal.WindowsPrincipal] [Security.Principal.WindowsIdentity]::GetCurrent()
+If (-not $identity.IsInRole([Security.Principal.WindowsBuiltInRole] 'Administrator')) {
+  Write-Warning "No administrator privilege."
+  Break
+}
+$pathItems = @()
+((Get-ItemProperty -LiteralPath $registryEnvironment).Path -split ";") + $addingPathItems |
+  Where-Object {$pathItems -notcontains $_} |
+  ForEach-Object {$pathItems += $_}
+$pathItems | ForEach-Object {Write-Output $_}
+Set-ItemProperty -LiteralPath $registryEnvironment -Name 'Path' –Value ($pathItems -join ";")
